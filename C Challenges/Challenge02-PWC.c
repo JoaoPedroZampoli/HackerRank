18b38b1cb415be63021b154b473a1219a736119c
João Pedro Zampoli <jpszampoli53@gmail.com>
2024-04-22 17:58:52 -0300

    First Code Commit - C Challenges

---
+++ C Challenges/Challenge02-PWC.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define MAX_LEN 100
+
+int main() 
+{
+    char ch, s[MAX_LEN], sen[MAX_LEN]; 
+    scanf("%c", &ch);
+    scanf("%s", s);
+    scanf("\n");
+    fgets(sen, MAX_LEN, stdin);
+    printf("%c", ch);
+    printf("\n%s", s);
+    printf("\n%s", sen);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
